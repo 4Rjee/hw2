dd332f1dd9b9c13f3ab72b008c709f7186b20658
4Rjee <kochurin.nikita@gmail.com>
2020-06-09 20:15:30 +0300

    code style edits

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,6 +3,7 @@
 
 int main(int, char**) {
     std::string name;
+    // getting user's name from stdin
     std::getline(std::cin, name);
     std::cout << "Hello world from " << name << std::endl; 
     return 0;
