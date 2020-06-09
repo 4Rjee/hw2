43255c7b79517a6ad78c374f8c5e8dc48b04cf0b
4Rjee <kochurin.nikita@gmail.com>
2020-06-09 20:12:58 +0300

    edited hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 
 int main(int, char**) {
-    cout << "Hello, world!" << endl; 
+    string name;
+    getline(cin, name);
+    cout << "Hello world from " << name << endl; 
     return 0;
 }
